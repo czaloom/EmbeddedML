63404e352f0ccf01301888659bbff8adf96e589c
Charles Zaloom <czaloom4@gmail.com>
2019-07-06 14:00:06 -0400

    v1.3b
    Forking library into lightweight and full versions.

--- Source/embeddedML.h
+++ Source/embeddedML.h
@@ -1,4 +1,7 @@
-/* EMBEDDEDML V1.2.1 */
+/* 
+ * EMBEDDEDML v1.3b
+ */
+
 /*
     embeddedML.h - Embedded Machine Learning Library
     Copyright (C) 2018  Charles Zaloom
@@ -14,28 +17,11 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with EmbeddedML.  If not, see <https://www.gnu.org/licenses/>
+    along with EmbeddedML. If not, see <https://www.gnu.org/licenses/>
 */
 
-#ifndef EMBEDDED_ML
-#define EMBEDDED_ML
-
-//#ifndef _EMBEDDED_
-//	#define _EMBEDDED_
-//#endif
-
-/*
- * NOTE: Defining _EMBEDDED_ removes all dependencies on standard libraries. 
- *       This means that some functions may not be available, but it will allow 
- *       usage on any system that runs C/C++.
- */
-
-
-#ifndef _EMBEDDED_
-    #include <stdlib.h>
-    #include <math.h>
-    #include <stdio.h>
-#endif
+#ifndef EMBEDDED_ML_METAL
+#define EMBEDDED_ML_METAL
 
 //-----ANN Structure-----
 typedef struct {
@@ -54,28 +40,19 @@ typedef struct {
     float (*hidden_activation_derivative)(float);
     
     float eta;      //Learning Rate
+    float beta;     //Bias Learning Rate
     float alpha;    //Momentum Coefficient
 } ANN;
 
 void train_ann(ANN *net, float *input, float *output);
 void run_ann(ANN *net, float *input);
+
 void init_ann(ANN *net);
-void init_embedded_ann(ANN *net);
-#ifndef _EMBEDDED_
-    void load_ann(ANN *net, const char *filename);   
-    void save_ann(ANN *net, const char *filename);
-#endif
+void init_pretrained_ann(ANN *net);
 
 //-----Utility-----
 void fill_zeros(float *v, unsigned int size);
 void fill_number(float *v, unsigned int size, float number);
-void strong_softmax(float *x, float *y);
-void weak_softmax(float *x, float *y);
-#ifndef _EMBEDDED_
-    void fill_rand(float *v, unsigned int size, float lower, float upper);
-    void softmax(unsigned int size, float multiplier, float *x, float *y);
-    void softmax2(unsigned int size, float multiplier, float *x, float *y);
-#endif
 
 //------Activation Functions-----
 float relu(float x);
@@ -83,13 +60,5 @@ float relu_derivative(float x);
 
 float relu2(float x);
 float relu2_derivative(float x);
-
-#ifndef _EMBEDDED_
-    //float tanhf(float x); //USE 'math.h' tanhf(x) function
-    float tanhf_derivative(float x);
-
-    float sigmoid(float x);
-    float sigmoid_derivative(float x);
-#endif
     
 #endif
